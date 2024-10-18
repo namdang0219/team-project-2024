dfe79e0f1b8d5a257ec392cb0d5dce99720d5621
namdang0219 <namdang.contact@gmail.com>
2024-10-18 10:42:07 +0900

    test connect to github

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>namehoh</Text>
     </View>
   );
 }
