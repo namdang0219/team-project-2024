2107b081e9d4a889c0deb573e9a74a69815aadaa
namdang0219 <namdang.contact@gmail.com>
2025-02-03 14:49:07 +0900

    update auth function in authstateslice and user data type

--- src/screen/app/global/LoadingScreen.tsx
+++ src/screen/app/global/LoadingScreen.tsx
@@ -2,11 +2,7 @@ import { View, ActivityIndicator, Dimensions } from "react-native";
 import React from "react";
 import { useTheme } from "@react-navigation/native";
 
-const { width } = Dimensions.get("window");
-
 const LoadingScreen = () => {
-	const { colors } = useTheme();
-
 	return (
 		<View
 			style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
